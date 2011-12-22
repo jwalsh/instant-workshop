ce75278df4af68357d7b70ed151b5a23e01cee99
Jason Walsh <github@jwalsh.net>
2011-12-21 18:26:46 -0800

    Adding Hiccup

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject instant-workshop "0.0.1"
   :dependencies
-  [[org.clojure/clojure "1.2.1"]
-   [compojure "1.0.0-RC2"]
+  [[compojure "1.0.0-RC2"]
+   [hiccup "0.3.6"]
+   [org.clojure/clojure "1.2.1"]
    [ring/ring-jetty-adapter "0.3.9"]])
